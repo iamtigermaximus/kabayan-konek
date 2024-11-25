d2066c8c77dfae1479cb7e060f5a61f22a87ec3d
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-11-25 18:05:14 +0200

    refactor: Refactor Advertisement component, created a separate styles files for advertise component.

---
+++ src/components/advertise/Advertise.styles.tsx
@@ -0,0 +1,309 @@
+'use client';
+
+import styled from 'styled-components';
+import { breakpoints as bp } from '../../utils/layout';
+
+export const Container = styled.div`
+  padding: 10px;
+  height: 100%;
+  z-index: 99;
+
+  @media (min-width: ${bp.md}) {
+    padding: 20px 50px;
+  }
+`;
+
+export const SectionContainer = styled.section`
+  padding: 10px;
+
+  @media (min-width: ${bp.lg}) {
+    padding: 20px 100px;
+  }
+`;
+
+export const DividerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 20px 0;
+`;
+
+export const DividerLine = styled.hr`
+  border: 0;
+  border-top: 1px solid #b4b4b4;
+  width: 100%;
+  margin: 0;
+  flex-grow: 1;
+`;
+
+export const DividerLabel = styled.span`
+  top: -12px;
+  background-color: white;
+  padding: 0 10px;
+  font-weight: bold;
+  color: #b4b4b4;
+  font-size: 1rem;
+  white-space: nowrap;
+
+  @media (min-width: ${bp.md}) {
+    font-size: 1.25rem;
+  }
+`;
+
+export const AdList = styled.div`
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 1fr;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+`;
+
+export const AdCard = styled.div`
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 15px;
+  background-color: #f9f9f9;
+  text-align: center;
+`;
+
+export const AdImage = styled.img`
+  width: 100%;
+  height: 200px;
+  object-fit: cover;
+  border-radius: 8px;
+  margin-bottom: 15px;
+`;
+
+export const AdTitle = styled.h3`
+  font-size: 1.25rem;
+  margin-bottom: 10px;
+`;
+
+export const AdDescription = styled.p`
+  font-size: 0.9rem;
+  color: #7f8c8d;
+`;
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  margin: 20px 0;
+`;
+
+export const PrevButton = styled.button`
+  padding: 8px 12px;
+  background-color: #222;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 100px;
+
+  &:disabled {
+    background-color: #888;
+    cursor: not-allowed;
+  }
+
+  &:hover:not(:disabled) {
+    background-color: tomato;
+  }
+`;
+
+export const NextButton = styled.button`
+  padding: 8px 12px;
+  background-color: #222;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 100px;
+
+  &:disabled {
+    background-color: #888;
+    cursor: not-allowed;
+  }
+
+  &:hover:not(:disabled) {
+    background-color: tomato;
+  }
+`;
+
+export const PageInfo = styled.span`
+  font-size: 1rem;
+  font-weight: bold;
+  color: #555;
+`;
+
+export const ModalContainer = styled.div`
+  /* position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0; */
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  /* background: rgba(0, 0, 0, 0.5); */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+`;
+
+export const ModalContent = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 400px;
+  max-width: 100%;
+  position: relative;
+  border: 0.5px solid rgba(0, 0, 0, 0.5);
+
+  @media (min-width: ${bp.md}) {
+    width: 600px;
+  }
+`;
+
+export const ModalContentTitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+`;
+
+export const ModalContentTitle = styled.h2`
+  font-size: 16px;
+`;
+
+export const ModalContentForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const FormItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 0;
+`;
+
+export const ModalCloseButton = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 16px;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+`;
+
+// export const CreateButtonContainer = styled.div`
+//   width: 100%;
+//   padding: 0 10px 10px;
+//   display: flex;
+//   justify-content: flex-end;
+//   align-items: center;
+// `;
+
+// export const CreateButton = styled.button`
+//   border: none;
+//   padding: 10px;
+//   min-width: 150px;
+//   max-width: 100%;
+//   background-color: #494848;
+//   color: white;
+// `;
+
+export const InputLabel = styled.label`
+  font-size: 14px;
+  padding: 5px 0;
+`;
+
+export const Input = styled.input`
+  font-size: 16px;
+  padding: 10px 2px;
+`;
+
+export const Textarea = styled.textarea`
+  width: 100%;
+  height: 200px;
+  overflow: auto;
+`;
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  /* padding: 0 10px; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+export const UploadButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const UploadButton = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: #494848;
+  color: white;
+  width: 100%;
+`;
+
+export const SubmitButtonContainer = styled.div`
+  width: 100%;
+  padding: 0 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const SubmitButton = styled.button`
+  border: none;
+  padding: 10px;
+  background-color: #494848;
+  color: white;
+  width: 100%;
+`;
+
+export const UploadedImageContainer = styled.div`
+  width: 100%;
+  height: 170px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+`;
+
+export const FilterSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 20px;
+`;
+
+export const FilterLabel = styled.label`
+  font-size: 1rem;
+  font-weight: bold;
+`;
+
+export const FilterSelect = styled.select`
+  padding: 10px;
+  font-size: 1rem;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+`;
