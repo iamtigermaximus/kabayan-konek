5cd8937fb5691a9340274b49088488b08ab3188b
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-12-02 04:28:56 +0200

    feat: Added a link to favicon so that when the user displays in search engines, it will display the website logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,15 @@ import { NextAuthProvider } from './providers/sessionProviders';
 import { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Kabayan Konek',
-  description: 'A platform connecting Filipinos in Finland.',
+  description:
+    'Kabayan Konek is a platform that connects Filipinos in Finland, offering lifestyle content, news, local events, a marketplace, and a community hub. Stay informed, share experiences, and engage with kababayans through our spotlight features and advertisements.',
   openGraph: {
     title: 'Kabayan Konek',
     description: 'A platform connecting Filipinos in Finland.',
     url: 'https://kabayankonek.com',
     images: [
       {
-        url: 'https://example.com/og-image.png',
+        url: 'https://res.cloudinary.com/dgkjr3qbc/image/upload/v1733010227/kabayan_iqasip.png',
         width: 1200,
         height: 630,
         alt: 'Kabayan Konek Logo',
@@ -34,28 +35,27 @@ export default function RootLayout({
           src="https://upload-widget.cloudinary.com/global/all.js"
           async
         ></script>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
-        <div>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-        </div>
         <NextAuthProvider>
           <Navbar />
           {children}
