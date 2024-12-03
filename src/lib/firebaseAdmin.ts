2dfa38022d31c5e9e977b112622616bbcfe53f59
Siegfred Gamboa <siegy.gamboa@gmail.com>
2024-12-03 03:47:05 +0200

    fix: Remove firebase notofication

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,17 +1,17 @@
-// lib/firebaseAdmin.ts
+// // lib/firebaseAdmin.ts
 
-import admin from 'firebase-admin';
+// import admin from 'firebase-admin';
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}'
-); // You should store this key in your environment variables securely
+// const serviceAccount = JSON.parse(
+//   process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}'
+// ); // You should store this key in your environment variables securely
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-} else {
-  admin.app(); // Use the default app
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// } else {
+//   admin.app(); // Use the default app
+// }
 
-export const messaging = admin.messaging();
+// export const messaging = admin.messaging();
